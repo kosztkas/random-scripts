c55001fc7bf3b687ca2e832498467f99f6497efe
Sandor Kosztka <kosztkas@users.noreply.github.com>
2017-07-26 10:42:14 +0200

    simple xml parser

---
+++ QuoteParser.ps1
@@ -0,0 +1,2 @@
+﻿[xml]$item = get-content quote.xml
+$item.SelectNodes('//EclipseLineItem')| where { $_.Description -ne "Factory integrated" } | select ProductNumber, Quantity, Description > hr_quote.txt
