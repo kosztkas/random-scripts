910c68ba6daddbccbe41d8c1c1fc011e0a3ff7b6
Sandor Kosztka <kosztkas@users.noreply.github.com>
2019-10-25 12:53:05 +0200

    Create getprice.py

---
+++ alibaba-prices/getprice.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import json
+import sys
+
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkecs.request.v20140526 import DescribePriceRequest
+
+
+def getPrice(instanceType, disk='0', unit = 'Month'):
+
+    # initialize the client
+    client = AcsClient( 'LTAIt7dM4K4FMBVW', 'U4rpjzp3qFjWdwcEGlcXIiXPQvKmXy','eu-central-1' )
+
+    # Create a request and set parameters
+    request = DescribePriceRequest.DescribePriceRequest()
+    request.set_InstanceType(instanceType)
+    request.set_PriceUnit(unit)
+    request.set_SystemDiskSize(disk)
+
+    # Initiate the request and print the response
+    response = client.do_action_with_exception(request)
+
+   # result = json.loads(json.dumps(response))
+   # print(result)
+
+    result = json.loads(response)
+    return result
