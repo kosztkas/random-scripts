cc03aa06bf1cb574d84b0add5302ac4d7149f4ec
Sandor Kosztka <kosztkas@users.noreply.github.com>
2023-10-08 16:23:33 +0200

    Update and rename QuoteParser.ps1 to PowerShell/QuoteParser.ps1

--- QuoteParser.ps1
+++
@@ -1,2 +0,0 @@
-﻿[xml]$item = get-content quote.xml
-$item.SelectNodes('//EclipseLineItem')| where { $_.Description -ne "Factory integrated" } | select ProductNumber, Quantity, Description > hr_quote.txt
