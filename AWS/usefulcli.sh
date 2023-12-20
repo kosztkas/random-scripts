3c740ba99dc8a0b390b1bd359a68a283d1af4582
Sandor Kosztka <kosztkas@users.noreply.github.com>
2023-12-20 11:14:33 +0100

    add ssm run command

--- AWS/usefulcli.sh
+++ AWS/usefulcli.sh
@@ -1,2 +1,8 @@
 # Get the instance ID + Name tage in a convenient format
 aws ec2 describe-instances --query "Reservations[*].Instances[*].{ID:InstanceId,Name:Tags[?Key=='Name']|[0].Value}" --output text
+
+# Run a command on linux instances from awscli via SSM
+aws ssm send-command --document-name "AWS-RunShellScript" --document-version "1" \
+--targets '[{"Key":"InstanceIds","Values":["i-1234567890abcde"]}]' \
+--parameters '{"workingDirectory":[""],"executionTimeout":["3600"],"commands":["echo "hello"]}' \
+--timeout-seconds 600 --max-concurrency "50" --max-errors "0" --region eu-central-1
