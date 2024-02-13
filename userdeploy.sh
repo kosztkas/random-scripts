2e12fc0420ea5eb1a527be7ab2605a201305703c
Sandor Kosztka <kosztkas@users.noreply.github.com>
2024-02-13 15:10:07 +0100

    Create userdeploy.sh

---
+++ userdeploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Setup variables
+groupname=cicakok
+groupid=666
+uid_base=1100
+users=(
+    aladar
+    bela
+    cica
+)
+
+groupadd -g $groupid $groupname
+
+cat << EOF > /etc/sudoers.d/$groupname-users
+## Allows people in group $groupname to run all commands without a password
+%$groupname ALL=(ALL:ALL) NOPASSWD: ALL
+EOF
+
+for user in "${users[@]}"; do
+
+    useradd -m --uid $((++uid_base)) -g $groupid $user
+
+    echo "$user is created"
+
+    mkdir -p /home/$user/.ssh
+    touch /home/$user/.ssh/authorized_keys
+
+    chown -R $uid_base:$groupid /home/$user/.ssh
+done
